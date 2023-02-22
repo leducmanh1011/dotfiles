ab2db5641cbaed61fc6703d960336d579ff997ad
ManhLD <leducmanh101198@gmail.com>
2023-02-22 10:50:29 +0700

    Nvim: add lualine.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,4 +8,4 @@ require("config/keymaps")
 require("config/options")
 
 -- vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme monokai_ristretto]]) --  oxocarbon, tokyonight, gruvbox, rose-pine, monokai
+vim.cmd([[colorscheme monokai]]) --  oxocarbon, tokyonight, gruvbox, rose-pine, monokai
