f9ffd2b33f8d833956855111777a131e97083f93
ManhLD <leducmanh101198@gmail.com>
2023-02-22 14:32:59 +0700

    Nvim: add alpha-nvim

---
+++ nvim/lua/plugins/alpha-nvim.lua
@@ -0,0 +1,55 @@
+-----------------------------------------------------------
+-- Dashboard configuration file
+-----------------------------------------------------------
+
+-- Plugin: alpha-nvim
+-- url: https://github.com/goolord/alpha-nvim
+
+-- For configuration examples see: https://github.com/goolord/alpha-nvim/discussions/16
+
+
+return {
+  {
+    "goolord/alpha-nvim",
+    config = function()
+      local dashboard = require("alpha.themes.dashboard")
+
+      -- Footer
+      local function footer()
+        local version = vim.version()
+        local print_version = "v" .. version.major .. "." .. version.minor .. "." .. version.patch
+        local datetime = os.date("%Y/%m/%d %H:%M:%S")
+
+        return print_version .. " - " .. datetime
+      end
+
+      -- Banner
+      local banner = {
+        "                                                    ",
+        " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+        " ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+        " ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+        " ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+        " ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+        " ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+        "                                                    ",
+      }
+
+      dashboard.section.header.val = banner
+
+      -- Menu
+      dashboard.section.buttons.val = {
+        dashboard.button("e", "  New file", ":ene <BAR> startinsert<CR>"),
+        dashboard.button("f", "  Find file", ":NvimTreeOpen<CR>"),
+        dashboard.button("s", "  Settings", ":e $MYVIMRC<CR>"),
+        dashboard.button("u", "  Update plugins", ":PackerUpdate<CR>"),
+        dashboard.button("q", "  Quit", ":qa<CR>"),
+      }
+
+      dashboard.section.footer.val = footer()
+
+
+      require("alpha").setup(dashboard.config)
+    end,
+  }
+}
