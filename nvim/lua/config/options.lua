aba92d98f88be63a87bfb011a0364807600c2548
ManhLD <leducmanh101198@gmail.com>
2023-02-24 17:00:46 +0700

    Nvim: update options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,13 +6,19 @@ local g = vim.g       -- Global variables
 local opt = vim.opt   -- Set options
 
 -- Neovim UI
+opt.mouse = "a"             -- Enable mouse support
 opt.number = true           -- Show line number
 opt.showmatch = true        -- Highlight matching parenthesis
-opt.colorcolumn = "80"      -- Line lenght marker at 80 columns
+opt.colorcolumn = "80"      -- Line lienght marker at 80 columns
+opt.cursorline = true       -- Hightlight cursor line
+opt.showmode = true         -- Show --MODE--
+opt.smartcase = true        -- Automatically match case if Capital letters are present
+opt.laststatus=3            -- Set global statusline
 
 -- Tabs, indent
 opt.expandtab = true        -- Use spaces instead of tabs
 opt.tabstop = 2             -- 1 tab == 2 spaces
+opt.autoindent = true       -- Uses the indent from the previous line
 opt.smartindent = true      -- Autoindent new lines
 opt.shiftwidth = 2          -- Shift 2 spaces when tab
 
