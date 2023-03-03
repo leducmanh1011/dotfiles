9c969e9233aa12be3ba1e3e2082a4c4b7da78207
ManhLD <leducmanh101198@gmail.com>
2023-03-03 14:55:09 +0700

    Nvim: add autocmds - auto update path when switch buffers

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,10 +3,10 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Import Lua modules
+require("config/autocmds")
 require("config/keymaps")
 require("config/options")
 require("config/lazy")
 
 -- vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme monokai]]) --  oxocarbon, tokyonight, gruvbox, rose-pine, monokai
-
