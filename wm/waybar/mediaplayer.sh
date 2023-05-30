8f8cc34108eecda1c6fdb2e84b64d2adde089f95
ManhLD <leducmanh101198@gmail.com>
2023-05-30 09:55:59 +0700

    Waybar: fix spotify not display song in top bar

--- wm/waybar/mediaplayer.sh
+++ wm/waybar/mediaplayer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-player_status=$(playerctl status 2> /dev/null)
+player_status=$(playerctl -p spotify status 2> /dev/null)
 if [ "$player_status" = "Playing" ]; then
     echo "$(playerctl metadata artist) - $(playerctl metadata title)"
 elif [ "$player_status" = "Paused" ]; then
