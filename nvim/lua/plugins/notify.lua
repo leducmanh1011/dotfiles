4ef01f49bb04b4de769f47a4869f3de5ecfd89a0
ManhLD <leducmanh101198@gmail.com>
2025-07-25 11:29:09 +0700

    Nvim: update config nvim-notify

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -5,7 +5,16 @@ return {
     local notify = require("notify")
     notify.setup({
       timeout = 3000,
-      background_colour = "#000000"
+      background_colour = "#000000",
+      stages = "fade_in_slide_out",
+      render = "default",
+      max_width = 80,
+      max_height = 10,
+      on_open = function(win)
+        if vim.api.nvim_win_is_valid(win) then
+          vim.api.nvim_win_set_config(win, { zindex = 100 })
+        end
+      end,
     })
     vim.notify = notify
   end,
